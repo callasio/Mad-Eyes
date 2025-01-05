149027f10ccf98791de035d0bf91cef9dbd140c9
Ryan Lee <ryan@sparcs.org>
2025-01-05 23:12:40 +0900

    back

--- Dockerfile
+++ Dockerfile
@@ -2,12 +2,13 @@ FROM python:3.11-slim
 
 WORKDIR /api
 
-COPY ./api/.env.local .env
+COPY ./api/.env .env
 
 COPY ./api/requirements.txt .
 RUN pip install -r requirements.txt
 
 COPY ./api . 
+RUN rm ./data.db
 
 EXPOSE 8080
 
--- web/src/api/utils/url.ts
+++ web/src/api/utils/url.ts
@@ -1,4 +1,5 @@
-const URL = "http://localhost:8080";
+// const URL = "http://localhost:8080";
+const URL = "https://madcamp-week2-1-34386751090.asia-northeast3.run.app";
 
 export function getUrl(...route: string[]): string {
   return `${URL}/${route.join("/")}`;
